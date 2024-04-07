87586c685eddd42147cab39c944ef34dcffef022
Alen Alduk <yostylez@gmail.com>
2024-04-07 15:00:37 +0200

    creating content upload

---
+++ content/signals.py
@@ -0,0 +1,18 @@
+from .models import Video
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
+import os
+
+@receiver(post_save, sender = Video)
+def video_post_save(sender, instance, created, **kwargs):
+    print('video uploaded')
+
+@receiver(post_delete, sender = Video)
+def video_post_delete(sender, instance, **kwargs):
+    """
+    Delets file from filesystem when corresponding `Video` object is deleted.
+    """
+    if instance.video_file:
+        if os.path.isfile(instance.video_file.path):
+            os.remove(instance.video_file.path)
+            print('video deleted.')
--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'users',
     'debug_toolbar',
     'django_rq',
-    'content'
+    'content.apps.ContentConfig'
     
 ]
 
