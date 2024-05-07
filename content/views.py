75f295f2465a933a42013491b48526f6bd869658
devXodia <youstylez@gmail.com>
2024-05-07 22:27:26 +0200

    feat: redis working

--- content/views.py
+++ content/views.py
@@ -11,6 +11,8 @@ from .models import Video
 from .serializers import VideoSerializer
 from django.views.decorators.cache import cache_page
 from django.contrib.auth.decorators import login_required
+from django.core.cache import cache
+
 # Create your views here.
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
@@ -18,24 +20,34 @@ CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 @api_view(['GET'])
 def stream_video_480p(request, title):
-    video = get_object_or_404(Video, title=title)
-    video_file_name = os.path.basename(video.video_file.path)
-    video_path = os.path.join(os.path.dirname(video.video_file.path), '480p', video_file_name.replace('.mp4', '_480p.m3u8'))
-    print('video_path: ', os.path.join(os.path.dirname(video.video_file.path), '480p'))
-    return FileResponse(open(video_path, 'rb'))
+    if request.method == 'GET': 
+        video = get_object_or_404(Video, title=title)
+        video_file_name = os.path.basename(video.video_file.path)
+        video_path = os.path.join(os.path.dirname(video.video_file.path), '480p', video_file_name.replace('.mp4', '_480p.m3u8'))
+        return FileResponse(open(video_path, 'rb'))
 
 
 @api_view(['GET'])
 def stream_video_720p(request, title):
-    video = get_object_or_404(Video, title=title)
-    video_path = video.video_file.path.replace('.mp4', '_720p.m3u8')
-    return FileResponse(open(video_path, 'rb'))
+    if request.method == 'GET': 
+        video = get_object_or_404(Video, title=title)
+        video_path = video.video_file.path.replace('.mp4', '_720p.m3u8')
+        return FileResponse(open(video_path, 'rb'))
+
 
 
 @cache_page(CACHE_TTL)
 @api_view(['GET'])
 def movie_list(request):
+    cache_key = request.path
+    cached_data = cache.get(cache_key)
     if request.method == 'GET':
-        videos = Video.objects.all()
-        serializer = VideoSerializer(videos, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK )
+        if not cached_data:
+            videos = Video.objects.all()
+            serializer = VideoSerializer(videos, many=True)
+            cached_data = serializer.data
+            cache.set(cache_key, cached_data, CACHE_TTL)
+        else:
+            return Response(cached_data, status=status.HTTP_200_OK)
+    
+        return Response(serializer.data, status=status.HTTP_200_OK)
